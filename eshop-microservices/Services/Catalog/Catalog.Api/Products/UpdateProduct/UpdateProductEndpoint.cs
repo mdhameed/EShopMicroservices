3a116638eb7e10f8f613f3e17f035e8757d7ebf9
Mohammed Hameed <hameed.in@hotmail.com>
2024-09-09 04:58:21 +0530

    Add some more Api Endpoints.

---
+++ eshop-microservices/Services/Catalog/Catalog.Api/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.Api.Products.UpdateProduct
+{
+    public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record UpdateProductResponse(bool IsSuccess);
+    public class UpdateProductEndpoint
+    {
+    }
+}
